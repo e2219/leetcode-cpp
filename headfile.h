65e027f23fc7a0adee2688c4bcdab34449a196ea
e2219 <3392677683@qq.com>
2026-03-07 20:06:45 +0800

    update difference_array

---
+++ headfile.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <ranges>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+#include <queue>
+#include <stack>
+#include <cmath>
