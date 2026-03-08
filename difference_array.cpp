9c5173727328305953f82d3d582274b048e628f8
e2219 <3392677683@qq.com>
2026-03-08 16:36:25 +0800

    update difference_array

--- difference_array.cpp
+++ difference_array.cpp
@@ -34,3 +34,35 @@ public:
         return ans;
     }
 };
+
+//leetcode 56 差分数组合并区间
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        int max_end = ranges::max(intervals, {}, [](const auto& a) { return a[1]; })[1]; //获取差分数组的最大范围
+        vector<vector<int>> ans;
+        int n = intervals.size();
+        vector<int> diff(max_end * 2 + 2);//注意区间范围，离散后的差分数组最大为 max_end * 2 + 1
+        for (auto & i : intervals) {
+            int l = i[0] * 2; //离散化数组，例如[1, 4],[5, 8]不离散的话会合并为一个区间[1, 8]，但实际上4, 5不相交。
+            int r = i[1] * 2; //这其实是一个由点到线的过程，把原本相邻的点（实际不合并）通过“*2离散”来分开
+            diff[l]++;
+            diff[r + 1]--;
+        }
+        int left = 0, right = 0, sum = 0, flag = 0;
+        for(int i = 0; i <= max_end * 2 + 1; i++) {
+            sum += diff[i]; //遍历差分数组：sum 由 0 变为正数是得到左端点， 从正数变为 0 是得到右端点的后一位
+            if (sum >= 1 && flag == 0) {
+                flag = 1;
+                left = i;
+            }
+            if (flag == 1 && sum == 0 || i == max_end * 2 + 1 && flag == 1) {
+                flag = 0;
+                right = i - 1;
+                ans.push_back({left / 2, right / 2}); //还原原数
+            }
+        }
+        return ans;
+    }
+};
